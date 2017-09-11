bd669ac5ec7a1a5c2ee5a5283a2c21ab32cdfcad
Yongjin Park <ypp@csail.mit.edu>
2017-09-11 08:22:46 -0400

    FDR results

--- make.pip.fdr.R
+++ make.pip.fdr.R
@@ -11,6 +11,16 @@ null.pip <- scan(null.file)
 alt.tab <- read.table(alt.file, sep = '\t', col.names = c('ensg', 'max.pip'))
 p.value <- empPvals(alt.tab$max.pip, null.pip)
 q.value <- qvalue(p.value)
+
+out.hdr <- gsub(pattern = '.gz', replacement = '', out.file)
+pdf(file = paste(out.hdr, '-pvalue.pdf', sep = ''))
+hist(p.value, 30)
+dev.off()
+
+pdf(file = paste(out.hdr, '-qvalue.pdf', sep = ''))
+plot(q.value)
+dev.off()
+
 out <- data.frame(alt.tab, p.val = p.value, q.val = q.value$qvalues)
 write.table(out, file = gzfile(out.file), quote = FALSE, row.names = FALSE,
             col.names = TRUE)
